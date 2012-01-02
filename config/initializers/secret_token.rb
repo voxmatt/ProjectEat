e1ca4cbfd0d16c2196280b4f20b88c974205deec
Matt Martin <matt@inbode.com>
2012-01-02 15:32:39 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProjectEat::Application.config.secret_token = '5037f83f71594821ced240ccc5f6e3cd3b5d2d91935e8709ca07960c978f859ace1386bcd35f719841f7e9d9cf1fc2bc65ce65f14d31708f0686bff9aeed78be'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+ProjectEat::Application.config.session_store :cookie_store, :key => '_ProjectEat_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# ProjectEat::Application.config.session_store :active_record_store
