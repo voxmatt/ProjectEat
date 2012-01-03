cfe4b19ddf29c11780319ba56055122526cbf2c7
Matt Martin <matt@inbode.com>
2012-01-02 22:28:40 -0600

    Added Index List

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,7 @@
 class Restaurant < ActiveRecord::Base
+  # default ordering
+  default_scope :order => 'Name'
+  
+  # validation
   validates :Name, :presence => true, :uniqueness => true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 ProjectEat::Application.routes.draw do
+  get "guide/index"
+
   resources :reviews
 
   resources :restaurants
@@ -53,6 +55,7 @@ ProjectEat::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
+  root :to => 'guide#index', :as => 'guide'
 
   # See how all your routes lay out with "rake routes"
 
